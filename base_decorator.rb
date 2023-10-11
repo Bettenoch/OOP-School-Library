658fbf6ad7caa8ff2d1a8ffc4cc532e099c1aaed
Bett Enock <bettenock968@gmail.com>
2023-10-10 22:43:41 -0700

    add base decorator

---
+++ base_decorator.rb
@@ -0,0 +1,15 @@
+require './nameable'
+
+class Decorator < Nameable
+  
+  # instance methods
+  def initialize(nameable)
+    super()
+
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
